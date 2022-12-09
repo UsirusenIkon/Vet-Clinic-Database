4f535275481482114fcc8350e99d795ab76fa465
Usirusen Ikon <lexicon048@gmail.com>
2022-12-09 03:50:55 +0100

    Querying multiple tables

--- schema.sql
+++ schema.sql
@@ -10,3 +10,20 @@ CREATE TABLE animals (
     species varchar(100),
     PRIMARY KEY(id)
 );
+
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name varchar(100),
+    age integer,
+    PRIMARY KEY(id),
+);
+
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name varchar(100),
+    PRIMARY KEY(id),
+);
+
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD species_id BIGINT REFERENCES species(id);
+ALTER TABLE animals ADD owner_id BIGINT REFERENCES owners(id);
